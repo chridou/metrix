1968b96b397b4dc55c59c76cf0641da9c2a586d0
cdouven <christian.douven@zalando.de>
2018-02-20 07:21:47 +0100

    find items in snapshot by path

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,29 +1,9 @@
 //! Pulling data from the backend for monitoring
 use json::{stringify, stringify_pretty, JsonValue};
 
-pub struct JsonConfig {
-    /// Serialize `true` as `1` and `false` as `0`
-    pub make_booleans_ints: bool,
-
-    /// Configure pretty JSON output.
-    ///
-    /// Produce pretty JSON with the given indentation if `Some(indentation)`.
-    /// If `None` compact JSON is generated.
-    pub pretty: Option<u16>,
-}
-
-impl Default for JsonConfig {
-    fn default() -> JsonConfig {
-        JsonConfig {
-            make_booleans_ints: false,
-            pretty: None,
-        }
-    }
-}
-
 /// A `Snapshot` which contains measured values
 /// at a point in time.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Snapshot {
     pub items: Vec<(String, ItemKind)>,
 }
@@ -37,6 +17,17 @@ impl Default for Snapshot {
 impl Snapshot {
     pub fn push<K: Into<String>>(&mut self, k: K, v: ItemKind) {
         self.items.push((k.into(), v))
+    }
+
+    /// Find an item on a path with a given a separator.
+    pub fn find_with(&self, path: &str, separator: char) -> Option<&ItemKind> {
+        let path: Vec<&str> = path.split(separator).collect();
+        find_item_in_snapshot(self, &path[..])
+    }
+
+    /// Find an item on a path with a `/` as a separator.
+    pub fn find(&self, path: &str) -> Option<&ItemKind> {
+        self.find_with(path, '/')
     }
 
     /// Output JSON with default settings.
@@ -70,7 +61,66 @@ impl Snapshot {
     }
 }
 
-#[derive(Debug, Clone)]
+/// Finds an item in a `Snapshot`
+///
+/// `path` are the segments of the path.
+///
+/// Since a `Snapshot` may contain multiple items with the same name
+/// only the first found will be returned.
+///
+/// If a prefix of a path leads to a value that value is returned
+/// and the rest of the path is discarded.
+///
+/// # Example
+///
+/// ```
+/// use metrix::snapshot::*;
+///
+/// // a -> 23
+/// // b -> c-> 42
+///
+/// let snapshot = Snapshot {
+///     items: vec![
+///         ("a".to_string(), ItemKind::UInt(23)),
+///         ("b".to_string(), ItemKind::Snapshot(Snapshot {
+///             items: vec![
+///          ("c".to_string(), ItemKind::UInt(42)),
+///            ]})),
+///     ]};
+///
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["a"]), Some(&ItemKind::UInt(23)));
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["a", "x"]), Some(&ItemKind::UInt(23)));
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["b", "c"]), Some(&ItemKind::UInt(42)));
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["b", "c", "x"]), Some(&ItemKind::UInt(42)));
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["b", "", "c"]), None);
+/// assert_eq!(find_item_in_snapshot(&snapshot, &["x"]), None);
+/// assert_eq!(find_item_in_snapshot(&snapshot, &[""]), None);
+/// assert_eq!(find_item_in_snapshot::<&str>(&snapshot, &[]), None);
+/// ```
+pub fn find_item_in_snapshot<'a, T>(snapshot: &'a Snapshot, path: &[T]) -> Option<&'a ItemKind>
+where
+    T: AsRef<str>,
+{
+    if path.is_empty() {
+        return None;
+    };
+
+    if let Some(item) = snapshot
+        .items
+        .iter()
+        .find(|&&(ref name, _)| name == path[0].as_ref())
+        .map(|x| &x.1)
+    {
+        match *item {
+            ItemKind::Snapshot(ref snapshot) => find_item_in_snapshot(snapshot, &path[1..]),
+            ref other => Some(other),
+        }
+    } else {
+        None
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum ItemKind {
     /// Simply a `String` in the `Snapshot.
     ///
@@ -100,6 +150,26 @@ impl ItemKind {
             ItemKind::UInt(v) => v.into(),
             ItemKind::Int(v) => v.into(),
             ItemKind::Snapshot(ref snapshot) => snapshot.to_json_value(config),
+        }
+    }
+}
+
+pub struct JsonConfig {
+    /// Serialize `true` as `1` and `false` as `0`
+    pub make_booleans_ints: bool,
+
+    /// Configure pretty JSON output.
+    ///
+    /// Produce pretty JSON with the given indentation if `Some(indentation)`.
+    /// If `None` compact JSON is generated.
+    pub pretty: Option<u16>,
+}
+
+impl Default for JsonConfig {
+    fn default() -> JsonConfig {
+        JsonConfig {
+            make_booleans_ints: false,
+            pretty: None,
         }
     }
 }
