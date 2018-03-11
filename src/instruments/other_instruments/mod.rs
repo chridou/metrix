bbd4b9c9ccd70d5b185c9154a8e310a5c307bb29
chridou <chridou@users.noreply.github.com>
2018-03-11 20:29:40 +0100

    added a meter for values

--- src/instruments/other_instruments/mod.rs
+++ src/instruments/other_instruments/mod.rs
@@ -1,5 +1,155 @@
 //! Other instruments
 pub use self::last_occurrence_tracker::LastOccurrenceTracker;
+pub use self::value_meter::ValueMeter;
+
+mod value_meter {
+    use std::time::{Duration, Instant};
+
+    use metrics::metrics::{Meter as MMeter, StdMeter};
+
+    use instruments::{Instrument, Update, Updates};
+
+    use {Descriptive, PutsSnapshot};
+    use snapshot::{ItemKind, Snapshot};
+    use util;
+
+    /// A meter that is ticked by values instead of observations
+    pub struct ValueMeter {
+        name: String,
+        title: Option<String>,
+        description: Option<String>,
+        last_tick: Instant,
+        inner_meter: StdMeter,
+    }
+
+    impl ValueMeter {
+        pub fn new_with_defaults<T: Into<String>>(name: T) -> ValueMeter {
+            ValueMeter {
+                name: name.into(),
+                title: None,
+                description: None,
+                last_tick: Instant::now(),
+                inner_meter: StdMeter::default(),
+            }
+        }
+
+        pub fn name(&self) -> &str {
+            &self.name
+        }
+
+        pub fn set_name<T: Into<String>>(&mut self, name: T) {
+            self.name = name.into();
+        }
+
+        pub fn set_title<T: Into<String>>(&mut self, title: T) {
+            self.title = Some(title.into())
+        }
+
+        pub fn set_description<T: Into<String>>(&mut self, description: T) {
+            self.description = Some(description.into())
+        }
+
+        fn put_values_into_snapshot(&self, into: &mut Snapshot) {
+            if self.last_tick.elapsed() >= Duration::from_secs(5) {
+                self.inner_meter.tick()
+            }
+
+            let snapshot = self.inner_meter.snapshot();
+
+            let meter_snapshot = MeterSnapshot {
+                one_minute: MeterRate {
+                    count: snapshot.count as u64,
+                    rate: snapshot.rates[0],
+                },
+                five_minutes: MeterRate {
+                    count: snapshot.count as u64,
+                    rate: snapshot.rates[1],
+                },
+                fifteen_minutes: MeterRate {
+                    count: snapshot.count as u64,
+                    rate: snapshot.rates[2],
+                },
+            };
+            meter_snapshot.put_snapshot(into);
+        }
+    }
+
+    impl Instrument for ValueMeter {}
+
+    impl PutsSnapshot for ValueMeter {
+        fn put_snapshot(&self, into: &mut Snapshot, descriptive: bool) {
+            util::put_postfixed_descriptives(self, &self.name, into, descriptive);
+            let mut new_level = Snapshot::default();
+            self.put_values_into_snapshot(&mut new_level);
+            into.push(self.name.clone(), ItemKind::Snapshot(new_level));
+        }
+    }
+
+    impl Updates for ValueMeter {
+        fn update(&mut self, with: &Update) {
+            if self.last_tick.elapsed() >= Duration::from_secs(5) {
+                self.inner_meter.tick()
+            }
+
+            match *with {
+                Update::ObservationWithValue(v, _) => {
+                    if v <= ::std::i64::MAX as u64 {
+                        self.inner_meter.mark(v as i64)
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    impl Descriptive for ValueMeter {
+        fn title(&self) -> Option<&str> {
+            self.title.as_ref().map(|n| &**n)
+        }
+
+        fn description(&self) -> Option<&str> {
+            self.description.as_ref().map(|n| &**n)
+        }
+    }
+
+    struct MeterSnapshot {
+        pub one_minute: MeterRate,
+        pub five_minutes: MeterRate,
+        pub fifteen_minutes: MeterRate,
+    }
+
+    impl MeterSnapshot {
+        pub fn put_snapshot(&self, into: &mut Snapshot) {
+            let mut one_minute = Snapshot::default();
+            self.one_minute.put_snapshot(&mut one_minute);
+            into.items
+                .push(("one_minute".to_string(), ItemKind::Snapshot(one_minute)));
+            let mut five_minutes = Snapshot::default();
+            self.five_minutes.put_snapshot(&mut five_minutes);
+            into.items
+                .push(("five_minutes".to_string(), ItemKind::Snapshot(five_minutes)));
+            let mut fifteen_minutes = Snapshot::default();
+            self.fifteen_minutes.put_snapshot(&mut fifteen_minutes);
+            into.items.push((
+                "fifteen_minutes".to_string(),
+                ItemKind::Snapshot(fifteen_minutes),
+            ));
+        }
+    }
+
+    struct MeterRate {
+        pub rate: f64,
+        pub count: u64,
+    }
+
+    impl MeterRate {
+        fn put_snapshot(&self, into: &mut Snapshot) {
+            into.items.push(("rate".to_string(), self.rate.into()));
+            into.items.push(("count".to_string(), self.count.into()));
+        }
+    }
+
+}
 
 mod last_occurrence_tracker {
     use std::time::Instant;
